8759a20ffe22c25053a5fcc920274e6389c6ef71
sergioeabarcaf <sergio.abarca@patagonia-it.com>
2020-08-20 22:43:23 -0400

    FEAT: create CounterApp component. Use spread operator in setCounters

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import { HookApp } from './HookApp';
+import { CounterApp } from './components/useState/CounterApp';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CounterApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
